8f689775fb186616c86cb230cf4bf00a4b0dbc1b
Jerry Main <ruiyang-jerry@outlook.com>
2023-03-14 21:46:57 -0700

    initial configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config'; // https://astro.build/config
 
 import solidJs from "@astrojs/solid-js";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs(), react()]
 });
